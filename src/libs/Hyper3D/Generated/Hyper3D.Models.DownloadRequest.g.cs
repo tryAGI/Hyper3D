3b80353fcd9a97b58f7d396db339a62e279d3576
Konstantin S. <havendv@gmail.com>
2026-05-11 01:57:00 +0400

    feat: Updated OpenAPI spec (#8)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Hyper3D/Generated/Hyper3D.Models.DownloadRequest.g.cs
+++ src/libs/Hyper3D/Generated/Hyper3D.Models.DownloadRequest.g.cs
@@ -40,5 +40,6 @@ namespace Hyper3D
         public DownloadRequest()
         {
         }
+
     }
 }
--- src/libs/Hyper3D/Generated/Hyper3D.Models.StatusResponseVariant2Job.g.cs
+++ src/libs/Hyper3D/Generated/Hyper3D.Models.StatusResponseVariant2Job.g.cs
@@ -49,5 +49,6 @@ namespace Hyper3D
         public StatusResponseVariant2Job()
         {
         }
+
     }
 }
