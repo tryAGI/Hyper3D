a7b2bbcf96a8cfcb80503b3e837cef2015a6aa12
Konstantin S. <havendv@gmail.com>
2026-09-13 20:06:56 +0400

    feat: Updated OpenAPI spec (#27)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Hyper3D/Generated/Hyper3D.AccountClient.g.cs
+++ src/libs/Hyper3D/Generated/Hyper3D.AccountClient.g.cs
@@ -34,7 +34,7 @@ namespace Hyper3D
         /// <inheritdoc/>
         public global::Hyper3D.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Hyper3D.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Hyper3D.AccountSourceGenerationContext.Default);
 
         /// <summary>
         ///
